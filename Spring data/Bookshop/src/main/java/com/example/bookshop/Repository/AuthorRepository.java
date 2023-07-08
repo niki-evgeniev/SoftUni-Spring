3210810fa8a956fa66bbc2e36b4ec7c91fb2f9ba
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-09 00:18:48 +0300

    Create AuthorRepository.java

---
+++ Spring data/Bookshop/src/main/java/com/example/bookshop/Repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.example.bookshop.Repository;
+
+import com.example.bookshop.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
