f24ea013ca288df001968bb4f1a5a2cf4747ce18
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-09 00:18:33 +0300

    Create Book.java

---
+++ Spring data/Bookshop/src/main/java/com/example/bookshop/entity/Book.java
@@ -0,0 +1,118 @@
+package com.example.bookshop.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "books")
+public class Book extends BaseEntity {
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+    @Enumerated
+    private EditionType editionType;
+    @Column
+    private BigDecimal price;
+    @Column
+    private int copies;
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+    @Enumerated
+    private AgeRestriction ageRestriction;
+    @ManyToOne
+    private Author author;
+    @ManyToMany
+    private Set<Category> categories;
+
+    public Book() {
+    }
+
+
+    public Book(EditionType editionType, LocalDate localDate, Integer copies, BigDecimal price, AgeRestriction ageRestriction, String title, Author author, Set<Category> categorySet) {
+        this.editionType = editionType;
+        this.releaseDate = localDate;
+        this.copies = copies;
+        this.price = price;
+        this.ageRestriction = ageRestriction;
+        this.title = title;
+        this.author = author;
+        this.categories = categorySet;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EditionType getEditionType() {
+        return editionType;
+    }
+
+    public void setEditionType(EditionType editionType) {
+        this.editionType = editionType;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public AgeRestriction getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(AgeRestriction ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categorySet) {
+        this.categories = categorySet;
+    }
+}
