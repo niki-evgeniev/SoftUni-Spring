6a09721bc64f02229ea54f1737a27182654d44b0
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-09 00:18:24 +0300

    Create Author.java

---
+++ Spring data/Bookshop/src/main/java/com/example/bookshop/entity/Author.java
@@ -0,0 +1,49 @@
+package com.example.bookshop.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class Author extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    private Set<Book> bookSet;
+
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBookSet() {
+        return bookSet;
+    }
+
+    public void setBookSet(Set<Book> bookSet) {
+        this.bookSet = bookSet;
+    }
+}
