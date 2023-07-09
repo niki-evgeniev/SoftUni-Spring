0dcf5c31c7084626b5f5dcb483ea1e5efef300af
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-10 01:48:04 +0300

    Create AppBeanConfiguration.java

---
+++ Spring data/Auto Mapping Objects Using Bookshop/src/main/java/com/example/bookshop/configoration/AppBeanConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.bookshop.configoration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
+
+@Configuration
+public class AppBeanConfiguration {
+
+    @Bean
+    public Scanner scanner() {
+        return new Scanner(System.in);
+    }
+
+
+}
