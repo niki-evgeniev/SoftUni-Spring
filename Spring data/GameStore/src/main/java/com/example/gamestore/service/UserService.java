f16fdaddc068e7e316ef9289dc35fce293e0adf7
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-11 00:10:12 +0300

    Create UserService.java

---
+++ Spring data/GameStore/src/main/java/com/example/gamestore/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.gamestore.service;
+
+import com.example.gamestore.model.dto.UserLoginDto;
+import com.example.gamestore.model.dto.UserRegisterDto;
+
+public interface UserService {
+
+    void registerUser(UserRegisterDto userRegisterDto);
+
+    void loginUser(UserLoginDto userLoginDto);
+
+    void logOut();
+}
