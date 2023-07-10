9d2fc6344e3f70ba242ea230c66bc4d016d94009
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-07-11 00:10:14 +0300

    Create ValidationUtil.java

---
+++ Spring data/GameStore/src/main/java/com/example/gamestore/util/ValidationUtil.java
@@ -0,0 +1,13 @@
+package com.example.gamestore.util;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Set;
+
+public interface ValidationUtil {
+
+    <E> boolean isValid (E entity);
+
+    <E> Set<ConstraintViolation<E>> violation(E entity);
+}
