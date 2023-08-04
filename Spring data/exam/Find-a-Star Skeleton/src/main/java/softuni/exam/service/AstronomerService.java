74ae20027cefaefe6b7fa2f79e97eb302933b54b
Nikolay Ivanov <106282292+niki-evgeniev@users.noreply.github.com>
2023-08-05 01:08:23 +0300

    Update AstronomerService.java

--- Spring data/exam/Find-a-Star Skeleton/src/main/java/softuni/exam/service/AstronomerService.java
+++ Spring data/exam/Find-a-Star Skeleton/src/main/java/softuni/exam/service/AstronomerService.java
@@ -3,13 +3,12 @@ package softuni.exam.service;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
-// TODO: Implement all methods
 public interface AstronomerService {
 
-    boolean areImported();
+boolean areImported();
 
-    String readAstronomersFromFile() throws IOException;
+String readAstronomersFromFile() throws IOException;
 
-	String importAstronomers() throws IOException, JAXBException;
+String importAstronomers() throws IOException, JAXBException;
 
 }
